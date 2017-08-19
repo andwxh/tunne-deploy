85177f7fbb74a14ad7f5d3c561b964e9895b31f2
andwxh <andwxh@gmail.com>
2017-08-19 17:40:52 +0800

    add build script for linux

--- udp2raw/build_linux.sh
+++ udp2raw/build_linux.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-cd $(dirname $0)
 
+cd $(dirname $0)
+wdir=$PWD
+
+
+# Preparing files
 mkdir -p git
 cd git
 git clone https://github.com/andwxh/udp2raw-tunnel.git
 cd udp2raw-tunnel
 git checkout 2698ec93951258d33543816c0a54eab3d1515f49
+tail -n +2 CMakeLists.txt >tmp
+mv tmp CMakeLists.txt
 
+
+# Build
+cmake .
+make
+
+
+mv udp2raw $wdir/udp2raw_linux
+cd $wdir
